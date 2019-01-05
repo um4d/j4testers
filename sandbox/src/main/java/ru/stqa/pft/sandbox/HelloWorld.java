9cf385bdaa5b4711b65577cf57db51504cdfc141
Timur Simonov <timur.simonov@gmail.com>
2019-01-05 12:34:53 +0300

    gitignore для директории build/

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -3,5 +3,9 @@ package ru.stqa.pft.sandbox;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("j4testers!");
+
+        System.out.println("test");
+
+
     }
 }
