8b09dae52dea691a74d7e951a8fd21058efcd63d
Timur Simonov <timur.simonov@gmail.com>
2019-03-14 11:09:49 +0300

    Фикс проверки состояния системы перед прохождением теста удаления контакта из группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactRemovingFromGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactRemovingFromGroup.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
 
-import com.google.common.collect.Comparators;
-import javafx.scene.Group;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
@@ -14,11 +12,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactRemovingFromGroup extends TestBase {
 
-    protected static ContactData contactBefore;
+    protected static ContactData contact;
     protected static GroupData group;
 
     @BeforeMethod
     public void ensurePreconditions() {
+
         group = app.db().groups().iterator().next();
         if (group == null) {
             app.goTo().groupPage();
@@ -28,16 +27,17 @@ public class ContactRemovingFromGroup extends TestBase {
                     .get().getId());
         }
 
-        contactBefore = app.db().getContactWithGroup(group);
-        if (contactBefore == null) {
+        contact = app.db().getContactWithGroup(group);
+
+        if (contact == null || contact.getGroups().equals(app.db().groups())) {
             app.goTo().HomePage();
-            contactBefore = new ContactData().withName("William")
+            contact = new ContactData().withName("William")
                     .withLname("Blake").withPhoneHome("111").withPhoneMobile("222")
                     .withPhoneWork("333").withEmail1("email@mail.com").withEmail2("444411@mmail.int")
                     .withEmail3("pr@").withAddress("Улица Пушкина, дом Колотушкина-2а")
                     .withGroup(group);
-            app.contacts().create(contactBefore);
-            contactBefore.withId(app.db().contacts().stream().max(Comparator.comparingInt(ContactData::getId))
+            app.contacts().create(contact);
+            contact.withId(app.db().contacts().stream().max(Comparator.comparingInt(ContactData::getId))
                     .get().getId());
         }
 
@@ -47,12 +47,11 @@ public class ContactRemovingFromGroup extends TestBase {
     public void testContactRemovingFromGroup() throws InterruptedException {
         app.goTo().HomePage();
         app.contacts().selectGroupForViewContacts(group);
-        Thread.sleep(5000);
-        app.contacts().selectById(contactBefore.getId());
+        app.contacts().selectById(contact.getId());
         app.contacts().submitRemovingFromGroup();
         ContactData contactAfter = app.db().contactByParameter("id",
-                Integer.toString(contactBefore.getId()));
-        assertThat(contactBefore.withoutGroup(group), equalTo(contactAfter));
+                Integer.toString(contact.getId()));
+        assertThat(contact.withoutGroup(group), equalTo(contactAfter));
     }
 
 
